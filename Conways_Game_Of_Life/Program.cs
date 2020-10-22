bf30956e5369caaa01b8e2354b903c8ea6d513b6
Jamie Kennedy <jamiekennedy97@gmail.com>
2020-10-22 10:31:34 +0100

    Initial Commit

---
+++ Conways_Game_Of_Life/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Conways_Game_Of_Life {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
